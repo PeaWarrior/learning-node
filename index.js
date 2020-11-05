b5d4205b0b00bf9583b8e8ee0f9ee7568e8f95b7
PeaWarrior <jacksonchen171@gmail.com>
2020-11-04 22:30:18 -0500

    added google oauth accessToken, refreshToken and profile for successful oauth

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express')
 const passport = require('passport')
-const GoogleStrategy = require('passport-google-oath20').Strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
 
 const app = express()
 
-passport.use(new GoogleStrategy())
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => {
+        console.log(accessToken)
+        console.log(refreshToken)
+        console.log(profile)
+    })
+)
+
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile']
+    })
+)
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
