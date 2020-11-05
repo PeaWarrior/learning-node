15c73ae8b5a24a2c40ee44ed8b9789dbe6cdc82f
PeaWarrior <jacksonchen171@gmail.com>
2020-11-05 16:55:35 -0500

    added Users model class

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const keys = require('./config/keys')
-
 require('./services/passport')
-const authRoutes = require('./routes/authRoutes')
+require('./models/User')
 
 mongoose.connect(keys.mongoURI)
 
 const app = express()
-authRoutes(app)
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
