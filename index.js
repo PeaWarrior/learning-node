58320a05ddcc87b5b707cd02d6a376f3d6095107
PeaWarrior <jacksonchen171@gmail.com>
2020-11-05 16:42:18 -0500

    connected mongo to index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
+
 require('./services/passport')
 const authRoutes = require('./routes/authRoutes')
+
+mongoose.connect(keys.mongoURI)
 
 const app = express()
 authRoutes(app)
