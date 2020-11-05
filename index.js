b5d080edc387fd0ef4e2e44848232aec21d04907
PeaWarrior <jacksonchen171@gmail.com>
2020-11-05 11:55:37 -0500

    refactored authRoutes and passport into seperate files

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
 const express = require('express')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const keys = require('./config/keys')
+require('./services/passport')
+const authRoutes = require('./routes/authRoutes')
 
 const app = express()
-
-passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-        console.log(accessToken)
-        console.log(refreshToken)
-        console.log(profile)
-    })
-)
-
-app.get(
-    '/auth/google', 
-    passport.authenticate('google', {
-        scope: ['profile']
-    })
-)
-
-app.get('/auth/google/callback', passport.authenticate('google'))
+authRoutes(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
---
+++ routes/authRoutes.js
@@ -0,0 +1,12 @@
+const passport = require('passport')
+
+module.exports = (app) => {
+    app.get(
+        '/auth/google', 
+        passport.authenticate('google', {
+            scope: ['profile']
+        })
+    )
+    
+    app.get('/auth/google/callback', passport.authenticate('google'))
+}
