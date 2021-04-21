04768879797b162f406f5e956995325b3f6ddbe3
PeaWarrior <jacksonchen171@gmail.com>
2021-04-21 02:33:20 -0400

    added writefile

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 require('dotenv').config();
 const http = require('http');
+const fs = require('fs');
 const { PORT } = process.env;
 
 const server = http.createServer((req, res) => {
   const url = req.url;
-  res.setHeader('Content-Type', 'text/html');
+  const method = req.method;
   if (url === '/') {
     res.write('<html>');
     res.write('<body><form action="/message" method="POST"><input type="text" /><button type="submit">Submit</button></form></body>');
     res.write('</html>');
-  } else {
-    res.write('<html>');
-    res.write('<body><h1>Hey</h1></body>');
-    res.write('</html>');
-  };
+    return res.end();
+  } 
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<body><h1>Hey</h1></body>');
+  res.write('</html>');
   res.end();
 });
 
