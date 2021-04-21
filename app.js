b6445dda6dfbb4560e26e5a9209989df80e5593d
PeaWarrior <jacksonchen171@gmail.com>
2021-04-21 03:17:14 -0400

    add buffer and write

--- app.js
+++ app.js
@@ -8,12 +8,20 @@ const server = http.createServer((req, res) => {
   const method = req.method;
   if (url === '/') {
     res.write('<html>');
-    res.write('<body><form action="/message" method="POST"><input type="text" /><button type="submit">Submit</button></form></body>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Submit</button></form></body>');
     res.write('</html>');
     return res.end();
   } 
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
+    const body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    });
+    req.on('close', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
     res.statusCode = 302;
     res.setHeader('Location', '/');
     return res.end();
