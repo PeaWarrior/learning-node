2f6e0b35b02c75d9774d84daa53918205253e78e
PeaWarrior <jacksonchen171@gmail.com>
2021-04-21 02:30:12 -0400

    add res

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const http = require('http');
 const { PORT } = process.env;
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  res.setHeader('Content-Type', 'text/html');
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<body><form action="/message" method="POST"><input type="text" /><button type="submit">Submit</button></form></body>');
+    res.write('</html>');
+  } else {
+    res.write('<html>');
+    res.write('<body><h1>Hey</h1></body>');
+    res.write('</html>');
+  };
+  res.end();
 });
 
 server.listen(PORT);
